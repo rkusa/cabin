6b1365456a4631e7dc069d06ce8abb826e4faf28
Markus Ast <m@rkusa.st>
2022-09-02 18:23:50 +0200

    impl View for tuples with up to 10 items

--- src/view.rs
+++ src/view.rs
@@ -4,18 +4,6 @@ pub trait View<A = ()> {
     fn render(self, out: impl Write) -> fmt::Result;
 }
 
-impl<V1, V2, A> View<A> for (V1, V2)
-where
-    V1: View<A>,
-    V2: View<A>,
-{
-    fn render(self, mut out: impl Write) -> fmt::Result {
-        self.0.render(&mut out)?;
-        self.1.render(&mut out)?;
-        Ok(())
-    }
-}
-
 impl<'a, A> View<A> for &'a str {
     fn render(self, mut out: impl Write) -> fmt::Result {
         out.write_str(self)?;
@@ -28,3 +16,27 @@ impl<A> View<A> for String {
         View::<A>::render(self.as_str(), out)
     }
 }
+
+macro_rules! impl_tuple {
+    ( $( $t:ident ),+ ;  $( $ix:tt ),* ) => {
+        impl<$( $t: View<A> ),*, A> View<A> for ($( $t, )*) {
+            fn render(self, mut out: impl Write) -> fmt::Result {
+                $(
+                    self.$ix.render(&mut out)?;
+                )*
+                Ok(())
+            }
+        }
+    };
+}
+
+impl_tuple!(V1; 0);
+impl_tuple!(V1, V2; 0, 1);
+impl_tuple!(V1, V2, V3; 0, 1, 2);
+impl_tuple!(V1, V2, V3, V4; 0, 1, 2, 3);
+impl_tuple!(V1, V2, V3, V4, V5; 0, 1, 2, 3, 4);
+impl_tuple!(V1, V2, V3, V4, V5, V6; 0, 1, 2, 3, 4, 5);
+impl_tuple!(V1, V2, V3, V4, V5, V6, V7; 0, 1, 2, 3, 4, 5, 6);
+impl_tuple!(V1, V2, V3, V4, V5, V6, V7, V8; 0, 1, 2, 3, 4, 5, 6, 7);
+impl_tuple!(V1, V2, V3, V4, V5, V6, V7, V8, V9; 0, 1, 2, 3, 4, 5, 6, 7, 8);
+impl_tuple!(V1, V2, V3, V4, V5, V6, V7, V8, V9, V10; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
