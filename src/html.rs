bd1698cd992fac801aefc6567f144756d748d8f5
Markus Ast <m@rkusa.st>
2023-01-28 19:36:11 +0100

    implement element type specific properties

--- src/html.rs
+++ src/html.rs
@@ -1,4 +1,5 @@
 mod attributes;
+pub mod elements;
 pub mod events;
 mod macros;
 
@@ -12,96 +13,94 @@ use serde::de::DeserializeOwned;
 use serde::Serialize;
 
 use self::attributes::{Attribute, Attributes};
-use self::events::InputEvent;
 use crate::component::registry::ComponentRegistry;
 use crate::render::{is_void_element, Renderer};
 pub use crate::view::text::{text, Text};
 use crate::view::{IntoView, View, ViewWrapper};
 
-pub fn div<V: View>(content: impl IntoView<V>) -> Html<V, ()> {
+pub fn div<V: View>(content: impl IntoView<V>) -> Html<V, (), ()> {
     custom("div", content)
 }
 
-pub fn ul<V: View>(content: impl IntoView<V>) -> Html<V, ()> {
+pub fn ul<V: View>(content: impl IntoView<V>) -> Html<V, (), ()> {
     custom("ul", content)
 }
 
-pub fn li<V: View>(content: impl IntoView<V>) -> Html<V, ()> {
+pub fn li<V: View>(content: impl IntoView<V>) -> Html<V, (), ()> {
     custom("li", content)
 }
 
-pub fn fieldset<V: View>(content: impl IntoView<V>) -> Html<V, ()> {
+pub fn fieldset<V: View>(content: impl IntoView<V>) -> Html<V, (), ()> {
     custom("fieldset", content)
 }
 
-pub fn button<V: View>(content: impl IntoView<V>) -> Html<V, ()> {
+pub fn button<V: View>(content: impl IntoView<V>) -> Html<V, (), ()> {
     custom("button", content)
 }
 
-pub fn input() -> Html<ViewWrapper<()>, ()> {
-    custom("input", ())
+pub fn input() -> Html<ViewWrapper<()>, (), elements::input::Input> {
+    create("input", ())
 }
 
-pub fn custom<V: View>(tag: &'static str, content: impl IntoView<V>) -> Html<V, ()> {
+pub fn custom<V: View>(tag: &'static str, content: impl IntoView<V>) -> Html<V, (), ()> {
     Html {
-        tag: HtmlTag {
-            tag,
-            attrs: (),
-            on_click: None,
-            on_input: None,
-        },
+        tag,
+        attrs: (),
+        on_click: None,
+        kind: (),
         content: content.into_view(),
     }
 }
 
-struct HtmlTag<A> {
+fn create<V: View, K: Default>(tag: &'static str, content: impl IntoView<V>) -> Html<V, (), K> {
+    Html {
+        tag,
+        attrs: (),
+        on_click: None,
+        kind: K::default(),
+        content: content.into_view(),
+    }
+}
+
+pub struct Html<V, A, K> {
     tag: &'static str,
     attrs: A,
     on_click: Option<(&'static str, String)>,
-    on_input: Option<&'static str>,
-}
-
-pub struct Html<V, A> {
-    tag: HtmlTag<A>,
+    kind: K,
     content: V,
 }
 
-impl<V, A> Html<V, A> {
+impl<V, A, K> Html<V, A, K> {
     pub fn attr<'a>(
         self,
         name: &'static str,
         value: impl Into<Cow<'a, str>>,
-    ) -> Html<V, impl Attributes + 'a>
+    ) -> Html<V, impl Attributes + 'a, K>
     where
         A: Attributes + 'a,
     {
         Html {
-            tag: HtmlTag {
-                tag: self.tag.tag,
-                attrs: Attribute::new(name, value, self.tag.attrs),
-                on_click: self.tag.on_click,
-                on_input: self.tag.on_input,
-            },
+            tag: self.tag,
+            attrs: Attribute::new(name, value, self.attrs),
+            on_click: self.on_click,
+            kind: self.kind,
             content: self.content,
         }
     }
 
-    pub fn class<'a>(self, value: impl Into<Cow<'a, str>>) -> Html<V, impl Attributes + 'a>
+    pub fn class<'a>(self, value: impl Into<Cow<'a, str>>) -> Html<V, impl Attributes + 'a, K>
     where
         A: Attributes + 'a,
     {
         Html {
-            tag: HtmlTag {
-                tag: self.tag.tag,
-                attrs: Attribute::new("class", value, self.tag.attrs),
-                on_click: self.tag.on_click,
-                on_input: self.tag.on_input,
-            },
+            tag: self.tag,
+            attrs: Attribute::new("class", value, self.attrs),
+            on_click: self.on_click,
+            kind: self.kind,
             content: self.content,
         }
     }
 
-    // TODO: not available for all tags (e.g. only for buttons)
     pub fn on_click<M, F: Future<Output = M>, P: Serialize + DeserializeOwned>(
         mut self,
         action: fn(M, P) -> F,
@@ -114,45 +113,35 @@ impl<V, A> Html<V, A> {
             // TODO: unwrap
             // TODO: delay serialization?
             let payload = serde_json::to_string(&payload).unwrap();
-            self.tag.on_click = Some((name, payload));
+            self.on_click = Some((name, payload));
         }
-        self
-    }
-
-    // TODO: not available for all tags (e.g. only for inputs)
-    pub fn on_input<M, F: Future<Output = M>>(mut self, action: fn(M, InputEvent) -> F) -> Self {
-        let name = ComponentRegistry::global().action_name(action as usize);
-        debug_assert!(name.is_some(), "action not registered");
-        self.tag.on_input = name;
         self
     }
 }
 
-impl<V, A> View for Html<V, A>
+impl<V, A, K> View for Html<V, A, K>
 where
     // TODO: remove `+ 'static` once removing away from boxed future
     V: View + Send + 'static,
     A: Attributes + Send + 'static,
+    K: Attributes + Send + 'static,
 {
     // TODO: move to `impl Future` once `type_alias_impl_trait` is stable
     type Future = Pin<Box<dyn Future<Output = Result<Renderer, fmt::Error>> + Send>>;
 
     fn render(self, r: Renderer) -> Self::Future {
         Box::pin(async move {
-            let mut el = r.element(self.tag.tag)?;
+            let mut el = r.element(self.tag)?;
 
-            if let Some((on_click, payload)) = &self.tag.on_click {
+            if let Some((on_click, payload)) = &self.on_click {
                 el.attribute("data-click", on_click)?;
                 el.attribute("data-click-payload", payload)?;
             }
 
-            if let Some(on_input) = &self.tag.on_input {
-                el.attribute("data-input", on_input)?;
-            }
+            self.attrs.render(&mut el)?;
+            self.kind.render(&mut el)?;
 
-            self.tag.attrs.render(&mut el)?;
-
-            if !is_void_element(self.tag.tag) {
+            if !is_void_element(self.tag) {
                 el.content(self.content).await
             } else {
                 el.end()
---
+++ src/html/elements/input.rs
@@ -0,0 +1,30 @@
+use std::future::Future;
+
+use crate::component::registry::ComponentRegistry;
+use crate::html::attributes::Attributes;
+use crate::html::events::InputEvent;
+use crate::html::Html;
+
+#[derive(Default)]
+pub struct Input {
+    on_input: Option<&'static str>,
+}
+
+impl<V, A> Html<V, A, Input> {
+    pub fn on_input<M, F: Future<Output = M>>(mut self, action: fn(M, InputEvent) -> F) -> Self {
+        let name = ComponentRegistry::global().action_name(action as usize);
+        debug_assert!(name.is_some(), "action not registered");
+        self.kind.on_input = name;
+        self
+    }
+}
+
+impl Attributes for Input {
+    fn render(&self, r: &mut crate::render::ElementRenderer) -> Result<(), std::fmt::Error> {
+        if let Some(on_input) = &self.on_input {
+            r.attribute("data-input", on_input)?;
+        }
+
+        Ok(())
+    }
+}
