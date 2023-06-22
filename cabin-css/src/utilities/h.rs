29ec18cc3dceef8dda157233fb771ef02a47cef2
Markus Ast <m@rkusa.st>
2023-06-22 11:17:06 +0200

    add h, max_h and min_h css utilities

---
+++ cabin-css/src/utilities/h.rs
@@ -0,0 +1,81 @@
+//! Set the element's height (`height`).
+//!
+//! <https://developer.mozilla.org/en-US/docs/Web/CSS/height>
+
+use crate::{Length, Property};
+
+const HEIGHT: &str = "height";
+
+/// `height: 0;`
+pub const ZERO: Property<Length> = Property(HEIGHT, Length::Px(0.0));
+
+/// `height: auto;`
+pub const AUTO: Property<Length> = Property(HEIGHT, Length::Auto);
+
+/// `height: 1px;`
+pub const PX: Property<Length> = Property(HEIGHT, Length::Px(1.0));
+
+/// `height: 100%;`
+pub const FULL: Property<Length> = Property(HEIGHT, Length::Percent(100.0));
+
+/// `height: 100vw;`
+pub const SCREEN: Property<Length> = Property(HEIGHT, Length::Vw(100));
+
+/// `height: min-content;`
+pub const MIN: Property<Length> = Property(HEIGHT, Length::MinContent);
+
+/// `height: max-content;`
+pub const MAX: Property<Length> = Property(HEIGHT, Length::MaxContent);
+
+/// `height: fit-content;`
+pub const FIT: Property<Length> = Property(HEIGHT, Length::FitContent);
+
+/// Multiple of `0.25rem` (`4px` by default): `height: {x * 0.25}rem;`
+pub fn unit(x: u16) -> Property<Length> {
+    Property(HEIGHT, Length::Rem(f32::from(x) * 0.25))
+}
+
+/// Multiple of `0.25rem` (`4px` by default): `height: {x * 0.25}rem;`
+pub fn unitf(x: f32) -> Property<Length> {
+    Property(HEIGHT, Length::Rem(x * 0.25))
+}
+
+/// `height: {x}rem;`
+pub fn rem(x: i16) -> Property<Length> {
+    Property(HEIGHT, Length::Rem(f32::from(x)))
+}
+
+/// `height: {x}rem;`
+pub fn remf(x: f32) -> Property<Length> {
+    Property(HEIGHT, Length::Rem(x))
+}
+
+/// `height: {x}px;`
+pub fn px(x: i16) -> Property<Length> {
+    Property(HEIGHT, Length::Px(f32::from(x)))
+}
+
+/// `height: {x}px;`
+pub fn pxf(x: f32) -> Property<Length> {
+    Property(HEIGHT, Length::Px(x))
+}
+
+/// `height: {x}%;`
+pub fn percent(x: i16) -> Property<Length> {
+    Property(HEIGHT, Length::Percent(f32::from(x)))
+}
+
+/// `height: {x}%;`
+pub fn percentf(x: f32) -> Property<Length> {
+    Property(HEIGHT, Length::Percent(x))
+}
+
+/// `height: {x}vw;`
+pub fn vw(x: u16) -> Property<Length> {
+    Property(HEIGHT, Length::Vw(x))
+}
+
+/// `height: {x}vh;`
+pub fn vh(x: u16) -> Property<Length> {
+    Property(HEIGHT, Length::Vh(x))
+}
---
+++ cabin-css/src/utilities/max_h.rs
@@ -0,0 +1,81 @@
+//! Set the element's maximum height (`max-height`).
+//!
+//! <https://developer.mozilla.org/en-US/docs/Web/CSS/max-height>
+
+use crate::{Length, Property};
+
+const MAX_HEIGHT: &str = "max-height";
+
+/// `max-height: 0;`
+pub const ZERO: Property<Length> = Property(MAX_HEIGHT, Length::Px(0.0));
+
+/// `max-height: auto;`
+pub const AUTO: Property<Length> = Property(MAX_HEIGHT, Length::Auto);
+
+/// `max-height: 1px;`
+pub const PX: Property<Length> = Property(MAX_HEIGHT, Length::Px(1.0));
+
+/// `max-height: 100%;`
+pub const FULL: Property<Length> = Property(MAX_HEIGHT, Length::Percent(100.0));
+
+/// `max-height: 100vw;`
+pub const SCREEN: Property<Length> = Property(MAX_HEIGHT, Length::Vh(100));
+
+/// `max-height: min-content;`
+pub const MIN: Property<Length> = Property(MAX_HEIGHT, Length::MinContent);
+
+/// `max-height: max-content;`
+pub const MAX: Property<Length> = Property(MAX_HEIGHT, Length::MaxContent);
+
+/// `max-height: fit-content;`
+pub const FIT: Property<Length> = Property(MAX_HEIGHT, Length::FitContent);
+
+/// Multiple of `0.25rem` (`4px` by default): `max-height: {x * 0.25}rem;`
+pub fn unit(x: u16) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Rem(f32::from(x) * 0.25))
+}
+
+/// Multiple of `0.25rem` (`4px` by default): `max-height: {x * 0.25}rem;`
+pub fn unitf(x: f32) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Rem(x * 0.25))
+}
+
+/// `max-height: {x}rem;`
+pub fn rem(x: i16) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Rem(f32::from(x)))
+}
+
+/// `max-height: {x}rem;`
+pub fn remf(x: f32) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Rem(x))
+}
+
+/// `max-height: {x}px;`
+pub fn px(x: i16) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Px(f32::from(x)))
+}
+
+/// `max-height: {x}px;`
+pub fn pxf(x: f32) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Px(x))
+}
+
+/// `max-height: {x}%;`
+pub fn percent(x: i16) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Percent(f32::from(x)))
+}
+
+/// `max-height: {x}%;`
+pub fn percentf(x: f32) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Percent(x))
+}
+
+/// `max-height: {x}vw;`
+pub fn vw(x: u16) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Vw(x))
+}
+
+/// `max-height: {x}vh;`
+pub fn vh(x: u16) -> Property<Length> {
+    Property(MAX_HEIGHT, Length::Vh(x))
+}
