a59d05b41cb96f57a75566ca1e749609fd46310f
Markus Ast <m@rkusa.st>
2023-07-11 15:30:21 +0200

    add box-sizing css utility

---
+++ cabin-css/src/utilities/box.rs
@@ -0,0 +1,17 @@
+//! Set how the browser should calculate an element's total size (`box-sizing`).
+//!
+//! <https://developer.mozilla.org/en-US/docs/Web/CSS/box-sizing>
+
+use crate::Property;
+
+const BOX_SIZING: &str = "box-sizing";
+
+/// ```css
+/// box-sizing: border-box;
+/// ```
+pub const BORDER: Property = Property(BOX_SIZING, "border-box");
+
+/// ```css
+/// box-sizing: content-box;
+/// ```
+pub const CONTENT: Property = Property(BOX_SIZING, "content-box");
