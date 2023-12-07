5f4e165036c8d66f932ff607569594c8c59ba52b
Markus Ast <m@rkusa.st>
2023-12-07 10:10:44 +0100

    add custom event attribute

--- src/html/events.rs
+++ src/html/events.rs
@@ -1,8 +1,14 @@
 use core::fmt;
 use std::borrow::Cow;
+use std::marker::PhantomData;
 use std::ops::Deref;
 
 use serde::{Deserialize, Serialize};
+
+use crate::error::InternalError;
+
+use super::attributes::Attributes;
+use super::elements::SerializeEventFn;
 
 #[derive(Default)]
 #[non_exhaustive]
@@ -61,5 +67,49 @@ impl From<InputValue> for Cow<'static, str> {
 impl fmt::Display for InputValue {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(&self.0)
+    }
+}
+
+pub struct CustomEvent<E> {
+    name: Cow<'static, str>,
+    event: Box<SerializeEventFn>,
+    marker: PhantomData<E>,
+}
+
+impl<E> CustomEvent<E> {
+    pub fn new(name: impl Into<Cow<'static, str>>, event: E) -> Self
+    where
+        E: serde::Serialize + 'static,
+    {
+        Self {
+            name: name.into(),
+            event: Box::new(move || {
+                use std::hash::{Hash, Hasher};
+
+                let mut hasher = twox_hash::XxHash32::default();
+                std::any::TypeId::of::<E>().hash(&mut hasher);
+                let hash = hasher.finish() as u32;
+                serde_json::to_string(&event)
+                    .map_err(|err| InternalError::Serialize {
+                        what: "on_click event",
+                        err,
+                    })
+                    .map(|json| (hash, json))
+            }),
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<E: 'static> Attributes for CustomEvent<E> {
+    fn render(self, r: &mut crate::render::ElementRenderer) -> Result<(), crate::Error> {
+        // TODO: directly write into el?
+        let (id, payload) = &(self.event)()?;
+        r.attribute(&format!("cabin-{}", self.name), id)
+            .map_err(crate::error::InternalError::from)?;
+        r.attribute(&format!("cabin-{}-payload", self.name), payload)
+            .map_err(crate::error::InternalError::from)?;
+
+        Ok(())
     }
 }
