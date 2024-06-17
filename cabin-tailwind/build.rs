b3d4e61f25afaa624f24b2602723dc3419d3084c
Markus Ast <m@rkusa.st>
2024-06-17 10:49:14 +0200

    add missing pink colors

--- cabin-tailwind/build.rs
+++ cabin-tailwind/build.rs
@@ -616,6 +616,10 @@ impl Default for Theme {
                 ("PINK_300", "#f9a8d4"),
                 ("PINK_400", "#f472b6"),
                 ("PINK_500", "#ec4899"),
+                ("PINK_600", "#db2777"),
+                ("PINK_700", "#be185d"),
+                ("PINK_800", "#9d174d"),
+                ("PINK_900", "#831843"),
             ],
             font_families: &[
                 (
