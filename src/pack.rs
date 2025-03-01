f43ce27824264f0882b4a2e2dcaa525a15e93a7f
Markus Ast <m@rkusa.st>
2025-03-01 11:11:28 +0100

    add generic conn to Pack trait

--- src/pack.rs
+++ src/pack.rs
@@ -1,39 +1,37 @@
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Debug, Clone)]
-pub enum Bound<T: Pack> {
+pub enum Bound<T: Pack<C>, C> {
     Packed(T::Packed),
     Unpacked(T),
 }
 
-impl<T: Pack> Bound<T> {
-    pub async fn unpack(self) -> Result<T, T::Error> {
+impl<T: Pack<C>, C> Bound<T, C> {
+    pub async fn unpack(self, conn: &C) -> Result<T, T::Error> {
         match self {
-            Bound::Packed(packed) => T::unpack(packed).await,
+            Bound::Packed(packed) => T::unpack(packed, conn).await,
             Bound::Unpacked(unpacked) => Ok(unpacked),
         }
     }
 }
 
-pub trait Pack: Sized {
+pub trait Pack<C>: Sized {
     type Packed: Serialize + DeserializeOwned;
     type Error;
 
     fn pack(&self) -> Self::Packed;
-    #[allow(async_fn_in_trait)]
-    async fn unpack(pack: Self::Packed) -> Result<Self, Self::Error>;
+    fn unpack(pack: Self::Packed, conn: &C) -> impl Future<Output = Result<Self, Self::Error>>;
 
-    fn packed(&self) -> Bound<Self> {
+    fn packed(&self) -> Bound<Self, C> {
         Bound::Packed(self.pack())
     }
 
-    fn unpacked(self) -> Bound<Self> {
+    fn unpacked(self) -> Bound<Self, C> {
         Bound::Unpacked(self)
     }
 }
 
-impl<T: Pack> Serialize for Bound<T> {
+impl<T: Pack<C>, C> Serialize for Bound<T, C> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -45,11 +43,38 @@ impl<T: Pack> Serialize for Bound<T> {
     }
 }
 
-impl<'de, T: Pack> Deserialize<'de> for Bound<T> {
+impl<'de, T: Pack<C>, C> Deserialize<'de> for Bound<T, C> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
         Ok(Bound::Packed(T::Packed::deserialize(deserializer)?))
+    }
+}
+
+impl<T, C> std::fmt::Debug for Bound<T, C>
+where
+    T: Pack<C> + std::fmt::Debug,
+    T::Packed: std::fmt::Debug,
+{
+    #[inline]
+    fn fmt(&self, f: &mut ::core::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Bound::Packed(packed) => f.debug_tuple("Packed").field(packed).finish(),
+            Bound::Unpacked(unpacked) => f.debug_tuple("Unpacked").field(unpacked).finish(),
+        }
+    }
+}
+
+impl<T, C> Clone for Bound<T, C>
+where
+    T: Pack<C> + Clone,
+    T::Packed: Clone,
+{
+    fn clone(&self) -> Self {
+        match self {
+            Bound::Packed(packed) => Bound::Packed(packed.clone()),
+            Bound::Unpacked(unpacked) => Bound::Unpacked(unpacked.clone()),
+        }
     }
 }
