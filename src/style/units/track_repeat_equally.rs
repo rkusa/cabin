161c94f8110d4a437bf989210f4fd997252075bf
Markus Ast <m@rkusa.st>
2025-12-18 12:37:25 +0100

    fix typo in track repeat style

--- src/style/units/track_repeat_equally.rs
+++ src/style/units/track_repeat_equally.rs
@@ -5,6 +5,6 @@ pub struct TrackRepeatEqually(pub u16);
 
 impl fmt::Display for TrackRepeatEqually {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "epeat({}, minmax(0, 1fr))", self.0)
+        write!(f, "repeat({}, minmax(0, 1fr))", self.0)
     }
 }
